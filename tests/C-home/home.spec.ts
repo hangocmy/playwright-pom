07fd759f442a3ad214d626ffe53246741a70db3d
hangocmy <hangocmy00@gmail.com>
2022-11-21 14:58:44 +0700

    edit hook

--- tests/C-home/home.spec.ts
+++ tests/C-home/home.spec.ts
@@ -1,27 +1,27 @@
-import { expect, test, Page } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { HeaderMenu, HomePage } from '../../pages/home-page';
 
-let page: Page;
+//let page: Page;
 
-test.beforeEach(async ({ browser }) => {
-  page = await browser.newPage();
+test.beforeEach(async ({ page }) => {
+  //page = await browser.newPage();
   await page.goto('/');
 });
 
 
-test.afterEach(async() => {
+test.afterEach(async({ page }) => {
   await page.close();
 });
 
 
 test.describe('Verify menu home page', () => {
-  test('Verify item in menu products', async() => {
+  test('Verify item in menu products', async({ page }) => {
     const headerMenu = new HeaderMenu(page);    
     await headerMenu.clickOnProductsMenu();
     await expect(headerMenu.productMenuDropdown).toContainText(["Live", "Automate", "Percy", "App Live", "App Automate"]);
   });
   
-  test('Verify item in menu developers', async() => {
+  test('Verify item in menu developers', async({ page }) => {
     const headerMenu = new HeaderMenu(page);
     await headerMenu.clickOnDevelopersMenu();
     await expect(headerMenu.developersMenuDropdown).toContainText(["Documentation", "Support", "Status", "Release Notes", "Open Source", "Events", "Test University", "Champions"]);
@@ -30,13 +30,13 @@ test.describe('Verify menu home page', () => {
 
 
 test.describe('Verify content in home page', () => {
-  test('Verify product cards heading', async() => {
+  test('Verify product cards heading', async({ page }) => {
     const homePage = new HomePage(page);
     await expect(homePage.productCards).toContainText(["live", "Automate", "Percy", "app live", "app automate"]);
   
   });
 
-  test('Verify menu footer', async () => {
+  test('Verify menu footer', async ({ page }) => {
     const homePage = new HomePage(page);
     
     await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
